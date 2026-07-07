f14f0bfc622212fe6ed4e419009202a8c03a5970
DrakeW17 <drake_wright@hotmail.com>
2026-07-07 13:51:32 -0700

    Commit
    The last commit didn't seem to update for some reason. This makes the same changes.

--- data_manager.gd
+++ data_manager.gd
@@ -8,9 +8,6 @@ const interactionTypeDistribution = [2, 2, 1]
 # A sum of interactionTypeDistribution
 var distributionSum = interactionTypeDistribution.reduce(func(accum, number): return accum + number, 0)
 
-# The amount of interactions of each type
-var interactionAmounts = [0, 0, 0]
-
 # A list of references to the NPC Situations
 var placedInteractions = []
 
@@ -19,15 +16,14 @@ var interactionTypesAvailable = []
 
 # Sets the interaction types
 func SetInteractionTypes() -> void:
-	# Appends 
+	# Sets interactionTypesAvailable
 	for i in range(3):
-			#interactionAmounts[i] = ceil((float(interactionTypeDistribution[i]) / distributionSum) * interactionAmount)
 			for s in range(ceil((float(interactionTypeDistribution[i]) / distributionSum) * interactionAmount)):
 				interactionTypesAvailable.append(i)
 	
+	# Distributes the available interaction types over the placed interactions
 	for i in range(placedInteractions.size()):
 		var typeID = randi_range(0, interactionTypesAvailable.size() - 1)
-		print(interactionTypesAvailable.size())
 		placedInteractions[i].NPC.type = interactionTypesAvailable[typeID]
 		placedInteractions[i].NPC.Set()
 		interactionTypesAvailable.remove_at(typeID)
