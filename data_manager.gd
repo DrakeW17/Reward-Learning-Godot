473a4372b995298f9c4b54213d62a17c64e870e9
DrakeW17 <drake_wright@hotmail.com>
2026-07-07 13:39:16 -0700

    Interaction Type Setting
    Interaction Type Setting done!

--- data_manager.gd
+++ data_manager.gd
@@ -1,9 +1,9 @@
 extends Node
 
-var interactionAmount = 10
+var interactionAmount = 5
 
 # The distribution of interaction types. Order: bad, neutral, good
-const interactionTypeDistribution = [2, 3, 2]
+const interactionTypeDistribution = [2, 2, 1]
 var distributionSum = interactionTypeDistribution.reduce(func(accum, number): return accum + number, 0)
 
 var interactionAmounts = [0, 0, 0]
@@ -13,13 +13,13 @@ var placedInteractions = []
 var interactionTypesAvailable = []
 
 func SetInteractionTypes() -> void:
-	for i in range(2):
-			interactionAmounts[i] = int((float(interactionTypeDistribution[i]) / distributionSum) * interactionAmount)
+	for i in range(3):
+			interactionAmounts[i] = ceil((float(interactionTypeDistribution[i]) / distributionSum) * interactionAmount)
 			for s in range(interactionAmounts[i]):
 				interactionTypesAvailable.append(i)
 	for i in range(placedInteractions.size()):
 		var typeID = randi_range(0, interactionTypesAvailable.size() - 1)
-		print(placedInteractions[i].NPC.type)
+		print(interactionTypesAvailable.size())
 		placedInteractions[i].NPC.type = interactionTypesAvailable[typeID]
 		placedInteractions[i].NPC.Set()
-		#interactionTypesAvailable.remove_at(typeID)
+		interactionTypesAvailable.remove_at(typeID)
